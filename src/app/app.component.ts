d48a3930f25a687e9572a2af79efd79c71c2a7b7
Vanessa Ortega <ortegavan@live.com>
2024-06-19 22:40:43 -0300

    feat: hero area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeroComponent, MenuComponent],
 })
 export class AppComponent {
     title = 'site2024';
